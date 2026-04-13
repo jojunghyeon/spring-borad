61d12a649b77302ff1f66099d4fd4dad332ed53d
jojunghyeon <jkjpmkjkj@gmail.com>
2026-04-13 23:06:58 +0900

    feat: Board 도메인 엔티티 생성 (id, 제목, 내용, 작성자)

--- src/main/java/com/board/spring_board/domain/Board.java
+++ src/main/java/com/board/spring_board/domain/Board.java
@@ -1,8 +1,31 @@
 package com.board.spring_board.domain;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
-
 public class Board {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String contents;
+    private String author;
+
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+
+    public String getContents() { return contents; }
+    public void setContents(String contents) { this.contents = contents; }
+
+    public String getAuthor() { return author; }
+    public void setAuthor(String author) { this.author = author; }
 }
